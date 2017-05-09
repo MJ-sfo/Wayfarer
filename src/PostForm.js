52b3e0e43c650743f97e1ded2a66af73c73c3bf0
janeosaur <janewie@gmail.com>
2017-05-08 21:56:56 -0700

    delete modal works

--- src/PostForm.js
+++ src/PostForm.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 
 class PostForm extends Component {
   constructor(props) {
--- src/PostIndiv.js
+++ src/PostIndiv.js
@@ -11,15 +11,23 @@ class PostIndiv extends Component {
       name: '',
       text: '',
       toBeUpdated: false,
-      showDialog: false // for modal
+      showDialog: false // for delete modal
     }
     this.deletePost = this.deletePost.bind(this);
     this.updatePost = this.updatePost.bind(this);
     this.handleNameChange = this.handleNameChange.bind(this);
     this.handleTextChange = this.handleTextChange.bind(this);
     this.handlePostUpdate = this.handlePostUpdate.bind(this);
+    this.handleDeletePost = this.handleDeletePost.bind(this);
   };
-  
+
+  handleNameChange(e) {
+    this.setState({ name: e.target.value });
+  }
+  handleTextChange(e) {
+    this.setState({ text: e.target.value });
+  }
+
   updatePost(e) {
     e.preventDefault();
     this.setState({ toBeUpdated: !this.state.toBeUpdated });
@@ -41,31 +49,21 @@ class PostIndiv extends Component {
 
   deletePost(e) {
     e.preventDefault();
+    this.setState({ showDialog: !this.state.showDialog})
+  }
+
+  handleDeletePost() {
     let id = this.props.uniqueID;
     this.props.onPostDelete(id);
-    console.log('oops deleted');
+    console.log('post was deleted through modal');
   }
 
-  handleTextChange(e) {
-    this.setState({ text: e.target.value });
-  }
-  handleNameChange(e) {
-    this.setState({ name: e.target.value });
-  }
-
-  handlePostUpdate(e) {
-    e.preventDefault();
-    let id = this.props.uniqueID;
-    let name = (this.state.name) ? this.state.name : null;
-    let text = (this.state.text) ? this.state.text : null;
-    let post = { name: name, text: text};
-    this.props.onPostUpdate(id, post);
-    this.setState({
-      toBeUpdated: !this.state.toBeUpdated,
-      name: '',
-      text: ''
-    })
-  }
+  // deletePost(e) {
+  //   e.preventDefault();
+  //   let id = this.props.uniqueID;
+  //   this.props.onPostDelete(id);
+  //   console.log('oops deleted');
+  // }
 
   render() {
     return (
@@ -100,22 +98,23 @@ class PostIndiv extends Component {
             </Col>
           </Row>
         </Grid>
-
         <div> {
-          this.state.showDialog &&
+          (this.state.showDialog) &&
           <ReactConfirmAlert
-            title="Confirm to Delete"
-            message="Are you sure to do this."
+            title="Confirm Delete"
+            message="Are you sure you want to delete this?"
             confirmLabel="Confirm"
             cancelLabel="Cancel"
-            onConfirm={() => alert('Action after Confirm')}
-            onCancel={() => alert('Action after Cancel')}
-          />
-        } </div>
-
+            onConfirm={this.handleDeletePost}
+            onCancel={() => this.setState({ showDialog: !this.state.showDialog})}
+          /> }
+        </div>
       </div>
     )
   }
 }
 
 export default PostIndiv;
+
+// on delete -> cancel & submit, error: index.js:44 Uncaught TypeError: Cannot read property 'parentNode' of null
+// how to incorporate ${this.props.name} in the delete confirmation modal message? 
