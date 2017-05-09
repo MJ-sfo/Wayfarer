503f080c9160bf003077e85ea7a6183702976e81
kwadrnt <kevinctse@gmail.com>
2017-05-09 08:38:32 -0700

    minor css

--- src/Home.js
+++ src/Home.js
@@ -23,7 +23,7 @@ class Home extends Component {
               <img className="iconImage" src={cityImage} alt={"cityImage"} height="120" width="120"/>
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </Col>
-            <Col sm={12} md={4}><br/><h3>Check the Reviews</h3>
+            <Col sm={12} md={4}><br/><h3>Spot the Reviews</h3>
               <img className="iconImage" src={reviewsImage} alt={"reviewsImage"} height="120" width="120"/>
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </Col>
