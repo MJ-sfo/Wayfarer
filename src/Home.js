1f6dbfdb32438a9515efcb6aabdf38ac915977bf
kwadrnt <kevinctse@gmail.com>
2017-05-11 11:47:48 -0700

    changed home css

--- src/Home.js
+++ src/Home.js
@@ -14,16 +14,16 @@ class Home extends Component {
         <h3 className="explore"> <Link to={'/cities/1'}>Explore</Link> </h3>
         <Grid>
           <Row className="show-grid">
-            <Col sm={12} md={4}><br/><h3>Locate Fav City</h3>
-              <img className="locateImage" src={locateImage} alt={"locateImage"} height="120" width="120"/>
+            <Col sm={12} md={2} mdOffset={3}><br/><h3>Locate Fav City</h3>
+              <img className="locateImage" src={locateImage} alt={"locateImage"} height="60" width="60"/>
               <p>Find your favorite city in our crowdsource.</p>
             </Col>
-            <Col sm={12} md={4}><br/><h3>Spot the Reviews</h3>
-              <img className="reviewImage" src={reviewImage} alt={"reviewImage"} height="120" width="120"/>
+            <Col sm={12} md={2}><br/><h3>Spot the Reviews</h3>
+              <img className="reviewImage" src={reviewImage} alt={"reviewImage"} height="60" width="60"/>
               <p>Remember a time during the last vacation that was memorable? Post it here!</p>
             </Col>
-            <Col sm={12} md={4}><br/><h3>Share Your Favs!</h3>
-              <img className="blogImage" src={blogImage} alt={"blogImage"} height="120" width="120"/>
+            <Col sm={12} md={2}><br/><h3>Share Your Favs!</h3>
+              <img className="blogImage" src={blogImage} alt={"blogImage"} height="60" width="60"/>
               <p>Blog about your favorite places!</p>
             </Col>
           </Row>
